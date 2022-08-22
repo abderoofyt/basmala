09daeac18d9f7c6ea67323262e29f04a0cbe3368
Junaid <abderoofyt@gmail.com>
2022-08-22 13:57:47 +0200

    update home

--- strings/urls.py
+++ strings/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('context/', views.context, name='context'),
     path('bismala/', views.bismala, name='bismala'),
+     path('bismala_list/', views.bismala_list, name='bismala_list'),
+    path('bismala_variable/', views.bismala_variable, name='bismala_variable'),
+    
 ]
--- strings/views.py
+++ strings/views.py
@@ -5,26 +5,43 @@ from django.shortcuts import render
 
 def strings(request):
     print('Hello, World!')
-    return HttpResponse()
+    console_log = "<script>console.log('Hello world!')</script>"
+    return HttpResponse(console_log)
 
 def hello(request):
     return HttpResponse("Hello, World!")
 
 def bismala(request):
-    return HttpResponse("\
-    <h1>Bismillah – بسم الله (In the name of Allah)</h1>\
-    <p>بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ</p>\
-    <p> bismillāhi r-raḥmāni r-raḥīm </p>\
-    <p>In the name of Allah, the Most Gracious, the most Merciful")
+    bismala = {
+        'arabic':"<p>بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ</p>",
+        'trans':"<p> bismillāhi r-raḥmāni r-raḥīm </p>",
+        'english':"<p>In the name of Allah, the Most Gracious, the most Merciful</p>"
+    }
+    return HttpResponse(bismala)
+
+def bismala_variable(request):
+    context = "<h3>(In the name of Allah)</h3>"
+    return HttpResponse("<h1>Bismillah </h1>"+"<h2>بسم الله <h2>"+context)
     
+
 def home(request):
     template = loader.get_template('home.html')
-    students = {
-        "Shamika":{'name':'Shamika', 'age': 25},
-        "Junaid":{'name':'Junaid', 'age':28}
+    bismala = {
+        'arabic':"<p>بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ</p>",
+        'trans':"<p> bismillāhi r-raḥmāni r-raḥīm </p>",
+        'english':"<p>In the name of Allah, the Most Gracious, the most Merciful</p>"
     }
-    return HttpResponse(template.render(students))
+    return HttpResponse(template.render(bismala))
 
+def bismala_list(request):
+    template = loader.get_template('list.html')
+    bismala = {
+        'arabic':"بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ",
+        'trans':"bismillāhi r-raḥmāni r-raḥīm ",
+        'english':"In the name of Allah, the Most Gracious, the most Merciful."
+    }
+    return HttpResponse(template.render(bismala))
+    
 def context(request):
     context = {
         "Shamika":{'name':'Shamika', 'age': 25},
